0ee308ee064d66cf988b0342c8d8d8723536490e
eloi <feloidev@gmail.com>
2023-10-21 08:39:35 -0300

    adding defer html

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserInputNumber(){
     return parseInt(userInput.value)
@@ -11,11 +12,24 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
 
 }
 
+function writeToLog(operatorIdentifier, prevResult, operationNumber, newResult){
+    const logEntry = {
+    operation: operatorIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult
+};
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 function add(num1, num2){
     const enteredNumber = getUserInputNumber();
     const initialResult = currentResult;
     currentResult += enteredNumber;
     createAndWriteOutput('+', initialResult, enteredNumber)
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+
 }
 
 function subtract(num1, num2){
@@ -23,18 +37,24 @@ function subtract(num1, num2){
     const initialResult = currentResult;
     currentResult -= enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber)
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+
 }
 function multiply(num1, num2){
     const enteredNumber = getUserInputNumber();
     const initialResult = currentResult;
     currentResult *= enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber)
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+
 }
 function divide(num1, num2){
     const enteredNumber = getUserInputNumber();
     const initialResult = currentResult;
     currentResult /= enteredNumber;
     createAndWriteOutput('/', initialResult, enteredNumber)
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+
 }
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
